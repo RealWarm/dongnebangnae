e0260fc8c9106ede9255ce1300fbdddcaa92c4de
Hoon <wjdgns8946@naver.com>
2025-06-13 17:00:59 +0900

    기본세팅

---
+++ src/main/java/com/dongnebangnae/portal/DongnebangnaeApplication.java
@@ -0,0 +1,13 @@
+package com.dongnebangnae.dongnebangnae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongnebangnaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongnebangnaeApplication.class, args);
+	}
+
+}
